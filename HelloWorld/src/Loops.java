202dcc0440af663ba943f99e33e90f82dd4eed0e
sandycampos <sandypcampos@gmail.com>
2022-11-25 23:15:12 -0400

    loops

--- HelloWorld/src/Loops.java
+++ HelloWorld/src/Loops.java
@@ -4,12 +4,12 @@ public class Loops {
 	public static void main(String[] args) {
 		
 		int numero = 1;
-		
-		while (numero < 1) {
+
+		while (numero <= 5) {
 			System.out.println(numero);
 			numero++;
 		}
-		
+
 		for (int n = 1; n <= 5; n++) {
 			System.out.println(n);
 		}
@@ -19,19 +19,16 @@ public class Loops {
 			System.out.println(numero);
 			numero++;
 		} while (numero < 1);
-		
-		String[] postagens = {"bom dia, amigos", "postagem 2", "postagem 3"};	
+		 
+		String[] postagens = { "bom dia, amigos", "postagem 2", "postagem 3" };
 		int numero = 0;
-		
+
 		// System.out.println(postagens.length);
-		
+
 		while (numero < postagens.length) {
 			System.out.println(postagens[numero]);
-			System.out.println("##### ");
 			numero++;
-			
 		}
-		*/
+ 		*/
 	}
-
 }
